9b30df8dedd264e650520474ddbad96dcb5f8b5c
Ricky Hu <ricky9667@gmail.com>
2020-03-23 00:03:18 +0800

    Add button and comments

--- app/src/main/java/com/example/hikerswatch/MainActivity.java
+++ app/src/main/java/com/example/hikerswatch/MainActivity.java
@@ -16,11 +16,14 @@ import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,32 +35,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // add <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" /> in manifest
+        // setting up locationManager and locationListener
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
+                // update location when location changes
                 updateLocation(location);
             }
 
             @Override
             public void onStatusChanged(String provider, int status, Bundle extras) {
-
             }
 
             @Override
             public void onProviderEnabled(String provider) {
-
             }
 
             @Override
             public void onProviderDisabled(String provider) {
-
             }
         };
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            // ask for permission when starting app if permission is not granted
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         } else {
+            // get location
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
             Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
@@ -80,12 +85,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void locationListening() {
+        // get location if permission granted
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
         }
     }
 
+    public void refresh(View view) {
+        // button for refreshing location
+
+        Toast.makeText(getApplicationContext(), "Updating Your Location...", Toast.LENGTH_SHORT).show();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            if (lastKnownLocation != null) {
+                updateLocation(lastKnownLocation);
+            }
+        }
+    }
+
     public void updateLocation(Location location) {
+        // updating location information
         Log.i("Location", location.toString());
 
         TextView latitudeText = findViewById(R.id.latitudeTextView);
@@ -122,7 +143,7 @@ public class MainActivity extends AppCompatActivity {
                 addressText.setText("Address:\n\n" + address);
 
             } else {
-                addressText.setText("Address:\n + You are fucking nowhere");
+                addressText.setText("Address: -");
             }
 
         } catch (Exception e) {
